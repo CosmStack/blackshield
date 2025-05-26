141cf71e2ee81b0ec399aeeab2c52dd3998ec2bb
Paulo Oliveira <contato@pholiveira.dev>
2025-05-25 22:16:39 -0300

    feat(eslint): add comprehensive ESLint security rules

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,17 +1,17 @@
-import { noUnsafeEnv } from './no-unsafe-env'
+import { noPublicSensitiveEnv } from './no-public-sensitive-env'
 import { noUnsafeHtml } from './no-unsafe-html'
 
 export const rules = {
-  'no-unsafe-env': noUnsafeEnv,
   'no-unsafe-html': noUnsafeHtml,
+  'no-public-sensitive-env': noPublicSensitiveEnv,
 }
 
 export const configs = {
   recommended: {
     plugins: ['@cosmstack/blackshield/eslint-plugin'],
     rules: {
-      '@cosmstack/blackshield/no-unsafe-env': 'error',
       '@cosmstack/blackshield/no-unsafe-html': 'error',
+      '@cosmstack/blackshield/no-public-sensitive-env': 'error',
     },
   },
 }
